a538f5dea251e29bdcb223848a558f030bb5ff12
pderks-eth <philipp.derks@lernende.ethz.ch>
2024-11-05 08:57:23 +0100

    Add initial implementation of DOM selection tasks

---
+++ DOM select/getElementes.js
@@ -0,0 +1,20 @@
+// 1. Aufgebe
+document.querySelectorAll("ul.games") [0]
+
+// 2. Aufgabe
+document.querySelectorAll("ul.games > li") [5]
+
+// 3. Aufgabe
+document.querySelectorAll("ul.games > li") [11]
+
+// 4. Aufgabe
+document.querySelectorAll("ul.games") [1]
+
+// 5. Aufgabe
+document.getElementsByTagName("h1") [0]
+
+// 6. Aufgabe
+document.getElementById("game-of-the-day")
+
+// 7. Aufgabe
+document.querySelectorAll(".sale")
